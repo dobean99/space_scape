67cc107d7ed92dd728dfa0037a0354a25a66c247
= <56818006+DoNgocKimDo@users.noreply.github.com>
2022-07-20 16:11:35 +0700

    add asset

---
+++ lib/models/space_ship.dart
@@ -0,0 +1,15 @@
+class SpaceShip {
+  final String name;
+  final String assetPath;
+  final int cost;
+  final int spriteId;
+  final double speed;
+
+  SpaceShip(
+      {required this.name,
+      required this.assetPath,
+      required this.cost,
+      required this.spriteId,
+      required this.speed});
+
+}
